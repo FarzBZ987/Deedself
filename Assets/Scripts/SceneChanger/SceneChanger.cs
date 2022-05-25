2f011fbbef1a11e11fe6b6829f5610f54734bc21
FarizAhmadBZ <fariz.berlian@gmail.com>
2022-05-25 14:31:20 +0700

    Moving to vlab
    There's some unfinished scripts

---
+++ Assets/Scripts/SceneChanger/SceneChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement
+
+public class SceneChanger : MonoBehaviour
+{
+    [HideInInspector] public bool isNewGame;
+    [HideInInspector] public int part;
+    [HideInInspector] public int level;
+
+    public void sceneChange()
+    {
+        if (isNewGame)
+        {
+            SceneManager.
+        }
+        else
+        {
+            
+        }
+    }
+}
