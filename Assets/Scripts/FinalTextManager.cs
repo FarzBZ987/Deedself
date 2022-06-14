c9324c9b316bbef812622b505ffce628e552e57f
FarizAhmadBZ <fariz.berlian@gmail.com>
2022-06-14 07:35:55 +0700

    Added Script for changing final message

---
+++ Assets/Scripts/FinalTextManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalTextManager : MonoBehaviour
+{
+    [SerializeField] private Text MessageText;
+    [SerializeField] private SpriteRenderer badgeObject;
+    [SerializeField] private int EndingPart;
+
+
+    [Header("Good ending")]
+    [SerializeField] private string GoodEndingText;
+    [SerializeField] private Sprite GoodEndingBadge;
+
+    [Header("Neutral Ending")]
+    [SerializeField] private string NeutralEndingText;
+    [SerializeField] private Sprite NeutralEndingBadge;
+
+    [Header("Bad Ending")]
+    [SerializeField] private string BadEndingText;
+    [SerializeField] private Sprite BadEndingBadge;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(GameData.instance != null)
+        {
+            if(GameData.instance.getScoring(EndingPart) == 0)
+            {
+                MessageText.text = BadEndingText;
+                badgeObject.sprite = BadEndingBadge;
+            }
+            if (GameData.instance.getScoring(EndingPart) == 1)
+            {
+                MessageText.text = NeutralEndingText;
+                badgeObject.sprite = NeutralEndingBadge;
+            }
+            if (GameData.instance.getScoring(EndingPart) == 2)
+            {
+                MessageText.text = GoodEndingText;
+                badgeObject.sprite = GoodEndingBadge;
+            }
+        }
+    }
+    
+}
