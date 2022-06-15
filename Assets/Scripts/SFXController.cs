5a9684ec26eba153a9e928d4a39966712a6c5af1
raitop <75676245+raitop@users.noreply.github.com>
2022-06-15 10:45:42 +0700

    Part 1 99% - Part 2 scene + MC placed

---
+++ Assets/Scripts/SFXController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SFXController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource SFXOutput;
+        [SerializeField] private AudioClip[] SFX;
+        
+        public void playSFX(int SFXIndex)
+        {
+            if(SFX.Length > 0) {
+                if(SFXIndex >= SFX.Length)
+                {
+                    SFXIndex = SFX.Length - 1;
+                }
+                if(SFXIndex < 0)
+                {
+                    SFXIndex = 0;
+                }
+                SFXOutput.PlayOneShot(SFX[SFXIndex]);
+            }   
+        }
+    }
+}
